e7dd520dcc667bc55ad4312771a9e512c9c10edb
hammadbabar2000 <33322170+hammadbabar2000@users.noreply.github.com>
2020-11-08 19:39:18 -0500

    Add files via upload

---
+++ colsion pop back/colsion pop back/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Tmrpicboxlright1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.tmrboxleft2 = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrboxup = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.tmrleft = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrright = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrup = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrdown = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Red
+        Me.PictureBox1.Location = New System.Drawing.Point(543, 213)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(29, 26)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Tmrpicboxlright1
+        '
+        Me.Tmrpicboxlright1.Enabled = True
+        Me.Tmrpicboxlright1.Interval = 1
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.DimGray
+        Me.PictureBox2.Location = New System.Drawing.Point(12, 142)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(29, 26)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.LimeGreen
+        Me.PictureBox3.Location = New System.Drawing.Point(262, 444)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(29, 26)
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'tmrboxleft2
+        '
+        Me.tmrboxleft2.Enabled = True
+        Me.tmrboxleft2.Interval = 1
+        '
+        'tmrboxup
+        '
+        Me.tmrboxup.Enabled = True
+        Me.tmrboxup.Interval = 1
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.PictureBox4.Location = New System.Drawing.Point(354, 336)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(29, 26)
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'tmrleft
+        '
+        Me.tmrleft.Interval = 1
+        '
+        'tmrright
+        '
+        Me.tmrright.Interval = 1
+        '
+        'tmrup
+        '
+        Me.tmrup.Interval = 1
+        '
+        'tmrdown
+        '
+        Me.tmrdown.Interval = 1
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 482)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Dont Collide; You Are Blue "
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Tmrpicboxlright1 As Timer
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents tmrboxleft2 As Timer
+    Friend WithEvents tmrboxup As Timer
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents tmrleft As Timer
+    Friend WithEvents tmrright As Timer
+    Friend WithEvents tmrup As Timer
+    Friend WithEvents tmrdown As Timer
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ colsion pop back/colsion pop back/Form1.vb
@@ -0,0 +1,92 @@
+﻿Public Class Form1
+    Private Sub Tmrpicboxleft_Tick(sender As Object, e As EventArgs) Handles Tmrpicboxlright1.Tick
+        PictureBox1.Left = PictureBox1.Left - 10
+        If PictureBox1.Right < 0 Then
+            PictureBox1.Left = Me.Width
+            PictureBox1.Top = Rnd(10) * Me.Height
+        End If
+
+    End Sub
+
+    Private Sub tmrboxright2_Tick(sender As Object, e As EventArgs) Handles tmrboxleft2.Tick
+        PictureBox2.Left = PictureBox2.Left - 10
+        If PictureBox2.Right < 0 Then
+            PictureBox2.Left = Me.Width
+            PictureBox2.Top = Rnd(10) * Me.Height
+        End If
+    End Sub
+    Private Sub tmrboxup_Tick(sender As Object, e As EventArgs) Handles tmrboxup.Tick
+        PictureBox3.Top = PictureBox3.Top - 10
+        If PictureBox3.Top < 0 Then
+            PictureBox3.Top = Me.Width
+            PictureBox3.Left = Rnd(10) * Me.Height
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Left
+                tmrleft.Enabled = True
+            Case Keys.Right
+                tmrright.Enabled = True
+            Case Keys.Up
+                tmrup.Enabled = True
+            Case Keys.Down
+                tmrdown.Enabled = True
+        End Select
+
+    End Sub
+
+    Private Sub tmrleft_Tick(sender As Object, e As EventArgs) Handles tmrleft.Tick
+        PictureBox4.Left = PictureBox4.Left - 10
+
+    End Sub
+
+    Private Sub tmrright_Tick(sender As Object, e As EventArgs) Handles tmrright.Tick
+        PictureBox4.Left = PictureBox4.Left + 10
+    End Sub
+
+    Private Sub tmrup_Tick(sender As Object, e As EventArgs) Handles tmrup.Tick
+        PictureBox4.Top = PictureBox4.Top - 10
+    End Sub
+
+    Private Sub tmrdown_Tick(sender As Object, e As EventArgs) Handles tmrdown.Tick
+        PictureBox4.Top = PictureBox4.Top + 10
+    End Sub
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        tmrdown.Enabled = False
+        tmrup.Enabled = False
+        tmrleft.Enabled = False
+        tmrright.Enabled = False
+    End Sub
+    Public Sub collision(a, b)
+        If a.Left >= b.Left - a.Width And a.Left <= b.Left + b.Width Then
+            If a.Top < b.Top + b.Height And a.Top > b.Top - a.Height Then
+                End
+            End If
+        End If
+    End Sub
+    Public Sub collision2(c, d)
+        If c.Left >= d.Left - c.Width And c.Left <= d.Left + d.Width Then
+            If c.Top < d.Top + d.Height And c.Top > d.Top - c.Height Then
+                End
+            End If
+        End If
+    End Sub
+    Public Sub collision3(e, f)
+        If e.Left >= f.Left - e.Width And e.Left <= f.Left + f.Width Then
+            If e.Top < f.Top + f.Height And e.Top > f.Top - e.Height Then
+                End
+            End If
+        End If
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Call collision(PictureBox4, PictureBox1)
+        Call collision2(PictureBox4, PictureBox2)
+        Call collision3(PictureBox4, PictureBox3)
+    End Sub
+End Class

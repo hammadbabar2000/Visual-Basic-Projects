48a36acb3f4fadd2d2986ef9c21adc032eeef5da
hammadbabar2000 <33322170+hammadbabar2000@users.noreply.github.com>
2020-11-08 19:42:29 -0500

    Add files via upload

---
+++ Word Proseser/Word Proseser/Form1.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SizeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem4 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem5 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem6 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem11 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem7 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColouerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RedToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BlueToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GreenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OrangeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BlackToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.YellowToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BrownToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StyeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BoldToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ItaliszedToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UnderlindedToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RegularToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(0, 27)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(716, 332)
+        Me.RichTextBox2.TabIndex = 1
+        Me.RichTextBox2.Text = ""
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SizeToolStripMenuItem, Me.ColouerToolStripMenuItem, Me.StyeToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(716, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SizeToolStripMenuItem
+        '
+        Me.SizeToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem2, Me.ToolStripMenuItem3, Me.ToolStripMenuItem4, Me.ToolStripMenuItem5, Me.ToolStripMenuItem6, Me.ToolStripMenuItem11, Me.ToolStripMenuItem7})
+        Me.SizeToolStripMenuItem.Name = "SizeToolStripMenuItem"
+        Me.SizeToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.SizeToolStripMenuItem.Text = "Size"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem2.Text = "10"
+        '
+        'ToolStripMenuItem3
+        '
+        Me.ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        Me.ToolStripMenuItem3.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem3.Text = "12"
+        '
+        'ToolStripMenuItem4
+        '
+        Me.ToolStripMenuItem4.Name = "ToolStripMenuItem4"
+        Me.ToolStripMenuItem4.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem4.Text = "16"
+        '
+        'ToolStripMenuItem5
+        '
+        Me.ToolStripMenuItem5.Name = "ToolStripMenuItem5"
+        Me.ToolStripMenuItem5.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem5.Text = "18"
+        '
+        'ToolStripMenuItem6
+        '
+        Me.ToolStripMenuItem6.Name = "ToolStripMenuItem6"
+        Me.ToolStripMenuItem6.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem6.Text = "20"
+        '
+        'ToolStripMenuItem11
+        '
+        Me.ToolStripMenuItem11.Name = "ToolStripMenuItem11"
+        Me.ToolStripMenuItem11.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem11.Text = "22"
+        '
+        'ToolStripMenuItem7
+        '
+        Me.ToolStripMenuItem7.Name = "ToolStripMenuItem7"
+        Me.ToolStripMenuItem7.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem7.Text = "24"
+        '
+        'ColouerToolStripMenuItem
+        '
+        Me.ColouerToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RedToolStripMenuItem, Me.BlueToolStripMenuItem, Me.GreenToolStripMenuItem, Me.OrangeToolStripMenuItem, Me.BlackToolStripMenuItem, Me.YellowToolStripMenuItem, Me.BrownToolStripMenuItem})
+        Me.ColouerToolStripMenuItem.Name = "ColouerToolStripMenuItem"
+        Me.ColouerToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.ColouerToolStripMenuItem.Text = "Color"
+        '
+        'RedToolStripMenuItem
+        '
+        Me.RedToolStripMenuItem.Name = "RedToolStripMenuItem"
+        Me.RedToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.RedToolStripMenuItem.Text = "Red"
+        '
+        'BlueToolStripMenuItem
+        '
+        Me.BlueToolStripMenuItem.Name = "BlueToolStripMenuItem"
+        Me.BlueToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.BlueToolStripMenuItem.Text = "Blue"
+        '
+        'GreenToolStripMenuItem
+        '
+        Me.GreenToolStripMenuItem.Name = "GreenToolStripMenuItem"
+        Me.GreenToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.GreenToolStripMenuItem.Text = "Green"
+        '
+        'OrangeToolStripMenuItem
+        '
+        Me.OrangeToolStripMenuItem.Name = "OrangeToolStripMenuItem"
+        Me.OrangeToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.OrangeToolStripMenuItem.Text = "Purple"
+        '
+        'BlackToolStripMenuItem
+        '
+        Me.BlackToolStripMenuItem.Name = "BlackToolStripMenuItem"
+        Me.BlackToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.BlackToolStripMenuItem.Text = "Black"
+        '
+        'YellowToolStripMenuItem
+        '
+        Me.YellowToolStripMenuItem.Name = "YellowToolStripMenuItem"
+        Me.YellowToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.YellowToolStripMenuItem.Text = "Yellow"
+        '
+        'BrownToolStripMenuItem
+        '
+        Me.BrownToolStripMenuItem.Name = "BrownToolStripMenuItem"
+        Me.BrownToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.BrownToolStripMenuItem.Text = "Brown"
+        '
+        'StyeToolStripMenuItem
+        '
+        Me.StyeToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BoldToolStripMenuItem, Me.ItaliszedToolStripMenuItem, Me.UnderlindedToolStripMenuItem, Me.RegularToolStripMenuItem})
+        Me.StyeToolStripMenuItem.Name = "StyeToolStripMenuItem"
+        Me.StyeToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.StyeToolStripMenuItem.Text = "Style"
+        '
+        'BoldToolStripMenuItem
+        '
+        Me.BoldToolStripMenuItem.Name = "BoldToolStripMenuItem"
+        Me.BoldToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.BoldToolStripMenuItem.Text = "Bold"
+        '
+        'ItaliszedToolStripMenuItem
+        '
+        Me.ItaliszedToolStripMenuItem.Name = "ItaliszedToolStripMenuItem"
+        Me.ItaliszedToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ItaliszedToolStripMenuItem.Text = "Italicize"
+        '
+        'UnderlindedToolStripMenuItem
+        '
+        Me.UnderlindedToolStripMenuItem.Name = "UnderlindedToolStripMenuItem"
+        Me.UnderlindedToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.UnderlindedToolStripMenuItem.Text = "Underline"
+        '
+        'RegularToolStripMenuItem
+        '
+        Me.RegularToolStripMenuItem.Name = "RegularToolStripMenuItem"
+        Me.RegularToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.RegularToolStripMenuItem.Text = "Regular"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(716, 362)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "ave"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents SizeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColouerToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StyeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem4 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem5 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem6 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem7 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem11 As ToolStripMenuItem
+    Friend WithEvents RedToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BlueToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GreenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OrangeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BlackToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents YellowToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BrownToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BoldToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ItaliszedToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UnderlindedToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RegularToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Word Proseser/Word Proseser/Form1.vb
@@ -0,0 +1,99 @@
+﻿Public Class Form1
+    Public Property SelectionFont As Font
+
+    'Close the program
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs)
+        End
+    End Sub
+    '/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    'Font size
+    'Font size 10
+    Private Sub ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem2.Click
+        Dim f As String
+        f = RichTextBox2.SelectionFont.Style
+        RichTextBox2.SelectionFont = New Font(f, 10, RichTextBox2.SelectionFont.Style)
+    End Sub
+    'Font size 12 
+    Private Sub ToolStripMenuItem3_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem3.Click
+        Dim f As String
+        f = RichTextBox2.SelectionFont.Style
+        RichTextBox2.SelectionFont = New Font(f, 12, RichTextBox2.SelectionFont.Style)
+    End Sub
+    'Font size 16
+    Private Sub ToolStripMenuItem4_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem4.Click
+        Dim f As String
+        f = RichTextBox2.SelectionFont.Style
+        RichTextBox2.SelectionFont = New Font(f, 16, RichTextBox2.SelectionFont.Style)
+    End Sub
+    'Font size 18
+    Private Sub ToolStripMenuItem5_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem5.Click
+        Dim f As String
+        f = RichTextBox2.SelectionFont.Style
+        RichTextBox2.SelectionFont = New Font(f, 18, RichTextBox2.SelectionFont.Style)
+    End Sub
+    'Font  size 20
+    Private Sub ToolStripMenuItem6_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem6.Click
+        Dim f As String
+        f = RichTextBox2.SelectionFont.Style
+        RichTextBox2.SelectionFont = New Font(f, 20, RichTextBox2.SelectionFont.Style)
+    End Sub
+    'Font  size 22
+    Private Sub ToolStripMenuItem7_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem7.Click
+        Dim f As String
+        f = RichTextBox2.SelectionFont.Style
+        RichTextBox2.SelectionFont = New Font(f, 22, RichTextBox2.SelectionFont.Style)
+    End Sub
+    'Font  size 24
+    Private Sub ToolStripMenuItem8_Click(sender As Object, e As EventArgs)
+        Dim f As String
+        f = RichTextBox2.SelectionFont.Style
+        RichTextBox2.SelectionFont = New Font(f, 24, RichTextBox2.SelectionFont.Style)
+    End Sub
+    '/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    'Color change
+    'Color change red
+    Private Sub RedToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RedToolStripMenuItem.Click
+        RichTextBox2.ForeColor = Color.Red
+    End Sub
+    'Color change blue
+    Private Sub BlueToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BlueToolStripMenuItem.Click
+        RichTextBox2.ForeColor = Color.Blue
+    End Sub
+    'Color change green
+    Private Sub GreenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GreenToolStripMenuItem.Click
+        RichTextBox2.ForeColor = Color.Green
+    End Sub
+    'Color change purple
+    Private Sub OrangeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OrangeToolStripMenuItem.Click
+        RichTextBox2.ForeColor = Color.Purple
+    End Sub
+    'Color change black
+    Private Sub BlackToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BlackToolStripMenuItem.Click
+        RichTextBox2.ForeColor = Color.Black
+    End Sub
+    'Color chaneg  yelow
+    Private Sub YellowToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles YellowToolStripMenuItem.Click
+        RichTextBox2.ForeColor = Color.Yellow
+    End Sub
+    'Color change brown
+    Private Sub BrownToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BrownToolStripMenuItem.Click
+        RichTextBox2.ForeColor = Color.Brown
+    End Sub
+    '/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    'Enable Bold
+    Private Sub BoldToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BoldToolStripMenuItem.Click
+        RichTextBox2.SelectionFont = New Font(RichTextBox2.SelectionFont, FontStyle.Bold)
+    End Sub
+    'Enable Italic
+    Private Sub ItaliszedToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ItaliszedToolStripMenuItem.Click
+        RichTextBox2.SelectionFont = New Font(RichTextBox2.SelectionFont, FontStyle.Italic)
+    End Sub
+    'Enable Underline
+    Private Sub UnderlindedToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UnderlindedToolStripMenuItem.Click
+        RichTextBox2.SelectionFont = New Font(RichTextBox2.SelectionFont, FontStyle.Underline)
+    End Sub
+    'Normal
+    Private Sub RegularToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RegularToolStripMenuItem.Click
+        RichTextBox2.SelectionFont = New Font(RichTextBox2.SelectionFont, FontStyle.Regular)
+    End Sub
+End Class
